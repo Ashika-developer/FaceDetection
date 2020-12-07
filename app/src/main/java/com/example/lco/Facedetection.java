8775025aaf198144588ff87985c9957d4bd005f5
Ashika <ashikamakki@gmail.com>
2020-12-07 20:42:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lco/Facedetection.java
@@ -0,0 +1,19 @@
+package com.example.lco;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class Facedetection extends Application {
+
+    public static final String RESULT_TEXT="RESULTTEXT";
+    public static final String RESULT_DIALOG="RESULTDIALOG";
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+
+    }
+}
